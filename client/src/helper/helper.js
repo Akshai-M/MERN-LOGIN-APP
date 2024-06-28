d4fa2e2d5ede490f925cfd62f20bfd1548613497
Akshai-M <mylapuramakshaikumar@gmail.com>
2024-06-28 12:46:41 +0530

    helper

--- client/src/helper/helper.js
+++ client/src/helper/helper.js
@@ -37,7 +37,10 @@ export async function getUser({ username }){
 /** register user function */
 export async function registerUser(credentials){
     try {
-        
+        const { data : { msg }, status } = await axios.post(`/api/register`, credentials);
+
+        let { username, email } = credentials;
+
         /** send email */
         if(status === 201){
             await axios.post('/api/registerMail', { username, userEmail : email, text : msg})
