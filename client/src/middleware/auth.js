879f0f596840974c0cf0ade57ec8a38aebee6f0a
Akshai-M <mylapuramakshaikumar@gmail.com>
2024-09-12 22:19:54 +0530

    modified

--- client/src/middleware/auth.js
+++ client/src/middleware/auth.js
@@ -3,7 +3,11 @@ import { Navigate } from "react-router-dom";
 import { useAuthStore } from "../store/store";
 
 export const AuthorizeUser = ({ children }) => {
-    
+    const token = localStorage.getItem('token');
+
+    if(!token){
+        return <Navigate to={'/'} replace={true}></Navigate>
+    }
 
     return children;
 }
