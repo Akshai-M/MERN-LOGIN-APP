77149c83efe95251c0b47eb6c8bb74641df3a730
Akshai-M <mylapuramakshaikumar@gmail.com>
2024-09-13 22:21:05 +0530

    convert

---
+++ client/src/helper/convert.js
@@ -0,0 +1,12 @@
+export default function convertToBase64(file){
+    return new Promise((resolve, reject) => {
+        const fileReader = new FileReader();
+        fileReader.readAsDataURL(file);
+
+       
+
+        fileReader.onerror = (error) => {
+            reject(error)
+        }
+    })
+}
