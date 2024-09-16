924ce4b7a4b46fedf51f2dec039bc4d52b369aee
Akshai-M <mylapuramakshaikumar@gmail.com>
2024-09-16 20:45:42 +0530

    UserModel

--- server/model/User.model.js
+++ server/model/User.model.js
@@ -6,6 +6,21 @@ export const UserSchema = new mongoose.Schema({
         required : [true, "Please provide unique Username"],
         unique: [true, "Username Exist"]
     },
+    password: {
+        type: String,
+        required: [true, "Please provide a password"],
+        unique : false,
+    },
+    email: {
+        type: String,
+        required : [true, "Please provide a unique email"],
+        unique: true,
+    },
+    firstName: { type: String},
+    lastName: { type: String},
+    mobile : { type : Number},
+    address: { type: String},
+    profile: { type: String}
     
 });
 
