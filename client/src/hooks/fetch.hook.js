a96d9306c4dae89c1375532cd7b0ef15d91713da
Akshai-M <mylapuramakshaikumar@gmail.com>
2024-09-19 21:59:17 +0530

    fetch

--- client/src/hooks/fetch.hook.js
+++ client/src/hooks/fetch.hook.js
@@ -13,7 +13,18 @@ export default function useFetch(query){
 
         const fetchData = async () => {
             try {
-                setData(prev => ({ ...prev, isLoading: true}));setData(prev => ({ ...prev, isLoading: false}));
+                setData(prev => ({ ...prev, isLoading: true}));
+
+                const { username } = !query ? await getUsername() : '';
+                
+                const { data, status } = !query ? await axios.get(`/api/user/${username}`) : await axios.get(`/api/${query}`);
+
+                if(status === 201){
+                    setData(prev => ({ ...prev, isLoading: false}));
+                    setData(prev => ({ ...prev, apiData : data, status: status }));
+                }
+
+                setData(prev => ({ ...prev, isLoading: false}));
             } catch (error) {
                 setData(prev => ({ ...prev, isLoading: false, serverError: error }))
             }
