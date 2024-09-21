10ab76738c4de056e94819aa2e8db53f8a350681
Akshai-M <mylapuramakshaikumar@gmail.com>
2024-09-21 22:17:42 +0530

    mailer

---
+++ server/controllers/mailer.js
@@ -0,0 +1,35 @@
+import nodemailer from 'nodemailer';
+import Mailgen from 'mailgen';
+
+import ENV from '../config.js';
+
+
+// https://ethereal.email/create
+let nodeConfig = {
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+        user: ENV.EMAIL, 
+        pass: ENV.PASSWORD, 
+    }
+}
+
+let transporter = nodemailer.createTransport(nodeConfig);
+
+let MailGenerator = new Mailgen({
+    theme: "default",
+    product : {
+        name: "Mailgen",
+        link: 'https://mailgen.js/'
+    }
+})
+
+/** POST: http://localhost:8080/api/registerMail 
+ * @param: {
+  "username" : "example123",
+  "userEmail" : "admin123",
+  "text" : "",
+  "subject" : "",
+}
+*/
