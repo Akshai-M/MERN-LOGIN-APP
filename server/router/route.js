255974d110c77a3ee9c0c4bb891ab212236a07c3
Akshai-M <mylapuramakshaikumar@gmail.com>
2024-11-18 23:35:45 +0530

    routes

--- server/router/route.js
+++ server/router/route.js
@@ -4,3 +4,9 @@ const router = Router();
 /** import all controllers */
 import * as controller from '../controllers/appController.js';
 import { registerMail } from '../controllers/mailer.js'
+import Auth, { localVariables } from '../middleware/auth.js';
+
+
+
+/** POST Methods */
+router.route('/register').post(controller.register); // register user
