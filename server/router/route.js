1ea2796f0ab182bef7c92a0510dae12113082824
Akshai-M <mylapuramakshaikumar@gmail.com>
2024-11-19 23:37:14 +0530

    routes

--- server/router/route.js
+++ server/router/route.js
@@ -13,3 +13,6 @@ router.route('/register').post(controller.register); // register user
 router.route('/registerMail').post(registerMail); // send the email
 router.route('/authenticate').post(controller.verifyUser, (req, res) => res.end()); // authenticate user
 router.route('/login').post(controller.verifyUser,controller.login); // login in app
+
+/** GET Methods */
+router.route('/user/:username').get(controller.getUser) // user with username
