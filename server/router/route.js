868238111f8b8c37425185a208b8201326a2b370
Akshai-M <mylapuramakshaikumar@gmail.com>
2024-11-21 23:12:44 +0530

    routes

--- server/router/route.js
+++ server/router/route.js
@@ -18,4 +18,4 @@ router.route('/login').post(controller.verifyUser,controller.login); // login in
 router.route('/user/:username').get(controller.getUser) // user with username
 router.route('/generateOTP').get(controller.verifyUser, localVariables, controller.generateOTP) // generate random OTP
 router.route('/verifyOTP').get(controller.verifyUser, controller.verifyOTP) // verify generated OTP
-router.route('/createResetSession').get(controller.createResetSession) 
+router.route('/createResetSession').get(controller.createResetSession) // reset all the variables
